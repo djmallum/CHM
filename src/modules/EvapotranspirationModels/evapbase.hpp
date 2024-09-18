770392e05ec6a800563d8bce83ee56add87c658d
Allum <donovan.allum@usask.ca>
2024-09-18 14:43:12 -0600

    Removed parameter struct as input from CalcEvapT

--- src/modules/EvapotranspirationModels/evapbase.hpp
+++ src/modules/EvapotranspirationModels/evapbase.hpp
@@ -3,15 +3,10 @@ class evapT_base
 public:
     virtual ~evapT_base() = default;
 
-    virtual void CalcEvapT(param_base& baseparam, var_base& basevar, model_output& output) = 0;
+    virtual void CalcEvapT(var_base& basevar, model_output& output) = 0;
     
     double delta(double& t);
     double gamma(double& P_atm, double& t);
-}
-
-struct param_base
-{
-
 }
 
 struct var_base
