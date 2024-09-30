54d6bdfcbd114083862f60d00edac77a0880dd25
Allum <donovan.allum@usask.ca>
2024-09-30 12:37:06 -0600

    Changes to PenmanMonteith ET model
    Three things:
    
    1. Set stomatal resistance to minimum by commenting out other
       calculation
    2. Made air_entry_tension calculation less ugly, ugliness was borrowed
       from CRHM
    3. Added safety to Aerodynamic resistance calculation in case wind
       measurement height is less than d.

--- src/modules/EvapotranspirationModels/PenmanMonteith.cpp
+++ src/modules/EvapotranspirationModels/PenmanMonteith.cpp
@@ -20,17 +20,25 @@ void PenmanMonteith::CalcHeights()
 
 double PenmanMonteith::CalcAeroResistance(const PM_vars& var)
 {
-     return pow( log((wind_measurement_height - d)/Z0),2) / 
-        (pow(kappa,2) * var.wind_speed);
+    if (wind_measurement_height - d > 0)
+    {
+        return pow( log((wind_measurement_height - d)/Z0),2) / 
+            (pow(kappa,2) * var.wind_speed);
+    }
+    else
+    {
+        return 0; // I don't know if this is right, but it is at least... safe.
+    }
 }
 
 double PenmanMonteith::CalcStomatalResistance(const PM_vars& var)
 {
     double rcstar = stomatal_resistance_min;
 
-    double leaf_area_index = Veg_height/Veg_height_max * 
-        (LAImin + seasonal_growth * (LAImax - LAImin));
-    rcstar = stomatal_resistance_min * LAImax / leaf_area_index;
+    // In CRHM, the below calculation is an option, for now just use the minimum option.
+//  double leaf_area_index = Veg_height/Veg_height_max * 
+//        (LAImin + seasonal_growth * (LAImax - LAImin));
+//    rcstar = stomatal_resistance_min * LAImax / leaf_area_index;
 
     double f1 = 1.0;
     if (var.short_wave_in > 0.0)
@@ -40,10 +48,7 @@ double PenmanMonteith::CalcStomatalResistance(const PM_vars& var)
     double f2 = std::max(1.0, 2.0 * (var.saturated_vapour_pressure - var.vapour_pressure) );
 //<double> (1.0, 2.0 * (var.saturated_vapour_pressure - var.vapour_pressure) );
 
-    double volumetric_soil_storage = (var.soil_storage/soil_depth + 
-           wilt_point) / porosity; 
-                                                       
-    double p = air_entry_tension * pow(1.0 / volumetric_soil_storage, pore_size_dist);  
+    double p = air_entry_tension * pow(porosity / (var.soil_storage/soil_depth + wilt_point), pore_size_dist);  
     
     double f3 = std::max(1.0, p/40.0);
 
