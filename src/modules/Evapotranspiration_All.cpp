a1f7db5834a6e34cbfe616c8a221d94d2d128629
Allum <donovan.allum@usask.ca>
2024-10-01 16:56:33 -0600

    Added PriestelyTaylor to ET module, also modified initialization of PT and PM models

--- src/modules/Evapotranspiration_All.cpp
+++ src/modules/Evapotranspiration_All.cpp
@@ -48,6 +48,14 @@ Evapotranspiration_All::Evapotranspiration_All(config_file cfg)
 
 void Evapotranspiration_All::init(mesh& domain)
 {
+    alpha = cfg.get("alpha_PriestelyTaylor",1.26);
+    wind_height = cfg.get("wind_measurement_height,2);
+    stomatal_resistance_min = cfg.get("stomatal_resistance_min",62);
+    soil_depth = cfg.get("soil_depth",1);
+    Frac_to_ground = cfg.get("Frac_to_ground",1); // iswr_subcanopy exists.
+                                                  
+    SoilDataObj = std::make_unique<Soil::soils_na>();
+
     for (size_t i = 0; i < domain->size_faces(); i++)
     {
         auto face = domain->face(i);
@@ -55,7 +63,10 @@ void Evapotranspiration_All::init(mesh& domain)
         
         // Consider if an if statement is necessary.
 
-        init_PenmanMonteith(d);
+
+        init_PenmanMonteith(d, face, wind_height, stomatal_resistance_min, soil_depth, Frac_to_ground);
+        
+        init_PriestelyTaylor(d,alpha);
 
         // put PristelyTaylor parameters here.
     }
@@ -69,9 +80,14 @@ void Evapotranspiration_All::run(mesh_elem& face)
 
     if (is_water(face))
     {
+        PT_vars my_PT_vars = set_PriestelyTaylor(face);
 
+        model_output output;
+
+        d.MyPriestelyTaylor->CalcEvapT(my_PT_vars,output);
         // Do PriestlyTaylor
     }
+    // TODO wetlands or saturated soils, need input from soils module.
     // else if (If wetlands)
     // {
     //     Also Do PriestlyTaylor
@@ -92,7 +108,7 @@ void Evapotranspiration_All::run(mesh_elem& face)
         d.MyPenmanMonteith->CalcEvapT(my_PM_vars,output);
     }
     
-    
+    // TODO total_ET, as well as PT ET and PM ET as separate. 
     (*face)["ET"_s] = output.ET;
     
 }
@@ -102,30 +118,47 @@ Evapotranspiration_All::~Evapotranspiration_All()
 
 }
 
-void Evapotranspiration_All::init_PenmanMonteith(Evapotransporation_All::data& d,mesh_elem& face)
+void Evapotranspiration_All::init_PriestelyTaylor(Evapotranspiration_All::data& d,double& alpha)
+{
+    d.MyPriestelyTaylor = std::make_unique<PriestelyTaylor>(alpha);
+}
+
+void Evapotranspiration_All::init_PenmanMonteith(Evapotransporation_All::data& d,mesh_elem& face, double& wind_height, double& stomatal_resistance_min, double& soil_depth, double& Frac_to_ground)
 {
     
     const std::string soil_type = cfg.get("soil_type","sand");
 
    
-    SoilDataObj = std::make_unique<Soil::soils_na>(); 
-    d.MyPenmanMonteith = std::make_unique<PenmanMonteith>(Atmosphere::Cp, Atmosphere::kappa,
-            SoilDataObj.air_entry_tension(soil_type),SoilDataObj.pore_size_dist(soil_type),
-            SoilDataObj.wilt_point(soil_type),SoilDataObj.porosity(soil_type));
+    
+    const double& Cp = Atmoshpere::Cp;
+    const double& kappa = Atmosphere::kappa;
+    const double& air_entry_tension = SoilDataObj.air_entry_tension(soil_type);
+    const double& pore_size_dist = SoilDataObj.pore_size_dist(soil_type);
+    const double& wilt_point = SoilDataObj.wilt_point(soil_type);
+    const double& porosity = SoilDataObj.porosity(soil_type);
 
-    if (face->has_vegetation()) 
+    // Leaf area index is not used if no vegetation, but LAI and LAImax are references in the PenmanMonteith model, therefore values are needed for initialization. It is ok if these values go out of scope as long as there is no vegetation. 
+    if (face->has_vegetation())
     {
-        d.MyPenmanMonteith->Veg_height = face->veg_attribute("CanopyHeight");
+        double& LAI = face->veg_attribute("LAI");
+        double& LAImax = face->veg_attribute("LAImax");
+
+        d.MyPenmanMonteith = std::make_unique<PenmanMonteith>(LAI, LAImax, Cp, kappa, 
+                air_entry_tension, pore_size_dist, wilt_point, porosity);
+
     }
-    //Only used if leaf_area_index is needed, for now it is commented out
-    //d.MyPenmanMonteith->Veg_height_max = cfg.get("Veg_height_max",1);
-    //d.MyPenmanMonteith->LAImin = cfg.get("Leaf_area_index_min",1);
-    //d.MyPenmanMonteith->LAImax = cfg.get("Lead_area_index_max",1);
-    //d.MyPenmanMonteith->seasonal_growth = cfg.get("seasonal_growth",1);
-    d.MyPenmanMonteith->wind_measurement_height = cfg.get("wind_measurement_height",2); // 2 is default and should be used if U_2m_above_srf is used
-    d.MyPenmanMonteith->stomatal_resistance_min = cfg.get("stomatal_resistance_min",1); // TODO get realistic minimum
-    d.MyPenmanMonteith->soil_depth = cfg.get("soil_depth",1);
-    d.MyPenmanMonteith->Frac_to_ground = cfg.get("Frac_to_ground",1); // iswr_subcanopy exists.
+    else
+    {
+        d.MyPenmanMonteith = std::make_unique<PenmanMonteith>(dummyvar, dummyvar, Cp, kappa, 
+                air_entry_tension, pore_size_dist, wilt_point, porosity);
+    }
+    
+    // Currently, one copy per triangle because that may be true later.
+    // For now it is uniform, so set a single copy per module instance (one per mpi process). 
+    d.MyPenmanMonteith->wind_measurement_height = wind_height // 2 is default and should be used if U_2m_above_srf is used
+    d.MyPenmanMonteith->stomatal_resistance_min = stomatal_resistance_min; // default based on Armstrong (2011) value, page 56.
+    d.MyPenmanMonteith->soil_depth = soil_depth;
+    d.MyPenmanMonteith->Frac_to_ground = Frac_to_ground; // iswr_subcanopy exists.
 
 }
 
@@ -137,3 +170,11 @@ PM_vars Evapotranspiration_All::set_PenmanMonteith_vars(mesh_elem& face,double& 
     return vars;
 }
 
+PT_vars Evapotransporaqtion_All:set_PriestelyTaylor_vars(mesh_elem& face)
+{
+    // TODO P_atm, is a state variable and should have a copy local to the run function 
+    // because it is calculated from the Atmosphere namespace, not done yet
+    PT_vars vars((*face)["netall"_s],(*face)["P_atm"]);
+
+    return vars;
+}
