14545f07faf7333a18d98756928b5ebbe72f3667
Allum <donovan.allum@usask.ca>
2024-10-08 16:26:19 -0600

    soil submodule initial commit

---
+++ src/modules/soil_submodules/soil_base.hpp
@@ -0,0 +1,22 @@
+#include <submodule_base.hpp>
+
+struct input_soil_base : input_base
+{
+
+};
+
+struct output_soil : output_base
+{
+    double soil_storage;
+};
+
+class soil_base : public submodule_base
+{
+public:
+    virtual soil_base = default;
+    virtual ~soil_base = default;
+
+    virtual void run(input_base& input, output_base& output) = 0;
+
+};
+
