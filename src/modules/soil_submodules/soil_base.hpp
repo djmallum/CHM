3337d56641284ae07f4045ca878da3e769ddd559
Allum <donovan.allum@usask.ca>
2024-10-16 16:57:39 -0600

    removed (de)constructor and arguments to virtual run function

--- src/modules/soil_submodules/soil_base.hpp
+++ src/modules/soil_submodules/soil_base.hpp
@@ -2,22 +2,10 @@
 
 #include <submodule_base.hpp>
 
-struct input_soil_base : input_base
-{
-};
-
-struct output_soil : output_base
-{
-    double soil_storage;
-};
-
 class soil_base : public submodule_base
 {
 public:
-    virtual soil_base = default;
-    virtual ~soil_base = default;
-
-    virtual void run(input_base& input, output_base& output) = 0;
+    virtual void run() = 0;
 
 };
 
