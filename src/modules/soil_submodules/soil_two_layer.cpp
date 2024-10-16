55d23091940e217acd0e6e71a01a1247c60584ba
Allum <donovan.allum@usask.ca>
2024-10-16 16:58:43 -0600

    Modified soil_two_layer to interact with a DTO
    DTO currently has every member as its own copy. This is memory
    inefficient in two ways:
    
    1. Output variables are held in memory even when the module is not run.
    
    2. Domain wide variables are not references to a single instance in the
       module (one per process rather than one per triangle)
    
    Reason 1 could be easily fixed by creating a sub struct to the DTO which
    is accessed through a pointer. That pointer is deleted at the end of
    each timestep.
    
    Reason 2 is not a problem for this module because there aren't any
    domain wide parameters. But it could be an issue for other modules.

--- src/modules/soil_submodules/soil_two_layer.cpp
+++ src/modules/soil_submodules/soil_two_layer.cpp
@@ -2,9 +2,8 @@
 
 #include "soil_two_layer.hpp"
 
-soil_two_layer::soil_two_layer(double& _num_layers) : num_layers(_num_layers)
+soil_two_layer::soil_two_layer(two_layer_DTO& _DTO) : DTO(_DTO)
 {
-    thaw_layer_fraction(num_layers);        
 };
 
 soil_two_layer::~soil_two_layer(void) 
@@ -12,228 +11,276 @@ soil_two_layer::~soil_two_layer(void)
 
 };
 
-void soil_two_layer::run(input_base& input, output_base& output) 
+void soil_two_layer::run() 
 {
-    const input_soil_two_layer& _input = static_cast<const input_soil_two_layer&>(input);
-    const output_soil& _output = static_cast<const output_soil&>(output);
 
-    set_layer_thaw_fraction(_input);
+    DTO.routing_residual = 0.0; // TODO zero'd because routing doesn't exist yet.
+                                // also would need to remove or use face_area
+    initialize_single_step_vars(); 
 
-    set_condensation(_input.ET,_output.condensation);
+    set_K_values();
 
-    organize_soil_layers(_input,_output);
+    set_layer_thaw_fraction();
 
-    set_detention_storage(_input,_output);
+    set_condensation();
 
-    set_depression_storage(_input,_output);
+    organize_soil_layers();
 
-    set_ground_water(_input,_output);
+    set_detention_storage();
 
-    set_subsurface_runoff(_input,_output);
+    set_depression_storage();
 
-    set_soil_ET(_input,_output);
+    set_ground_water();
+
+    set_subsurface_runoff();
+
+    set_soil_ET();
 
 };
 
-void soil_two_layer::organize_soil_layers(input_soil_two_layer& _input,output_soil_two_layer& _output);
+void soil_two_layer::initialize_single_step_vars()
 {
-    double excess = 0.0; // mm
+    DTO.condensation = 0.0;
+    DTO.actual_ET = 0.0;
+    DTO.soil_excess_to_runoff = 0.0;
+    DTO.soil_excess_to_gw = 0.0;
+    DTO.ground_water_outflow = 0.0;
+    DTO.soil_to_ssr = 0.0;
+};
+
+void soil_two_layer::set_K_values()
+{
+    k_estimator.run(DTO.K_soil_to_gw,DTO.K_rechr_to_ssr,DTO.K_lower_to_ssr,DTO.K_detention_snow_to_runoff,
+            DTO.K_detention_organic_to_runoff,DTO.K_depression_to_ssr);
+};
+
+void soil_two_layer::set_thaw_fraction()
+{
+    DTO.thaw_fraction_rechr = 0.0;
+    DTO.thaw_fraction_lower = 0.0;
+
+    if (DTO.thaw_front_depth == 0.0 && DTO.freeze_front_depth == 0.0)
+        DTO.thaw_fraction_rechr = 1.0;
+        DTO.thaw_fraction_lower = 1.0; 
+    else 
+    {
+
+        if (DTO.thaw_front_depth < DTO.rechr_depth)
+            DTO.thaw_fraction_rechr = DTO.thaw_front_depth / DTO.rechr_depth;
+        
+        else
+        {
+            DTO.thaw_fraction_rechr = 1.0;
+            DTO.thaw_fraction_lower = (DTO.thaw_front_depth - DTO.rechr_depth) 
+                / (DTO.soil_depth - DTO.rechr_depth);
+        }   
+    }
+    
+};
+
+void soil_two_layers::set_condensation()
+{
+    if (DTO.potential_T < 0.0)
+    {
+        DTO.condensation = -1.0 * DTO.potential_ET;
+        DTO.potential_ET = 0.0;
+    }
+};
+
+void soil_two_layer::organize_soil_layers();
+{
     if (soil_storage_max > 0.0)
     {
-        double soil_lower_storage = soil_storage - soil_rechr_storage;
+        double soil_lower_storage = DTO.soil_storage - DTO.soil_rechr_storage;
 
-        double potential = _input.infil + _output.condensation;
+        double potential = DTO.infil + DTO.condensation;
 
-        double possible = layer_thaw_fraction[0] * (soil_rechr_max - soil_rechr_storage);
+        double possible = DTO.thaw_fraction_rechr * (DTO.soil_rechr_max - DTO.soil_rechr_storage);
 
         if (possible > potential)
             possible = potential;
         else
-            soil_excess_to_runoff = potential - possible;
+            DTO.soil_excess_to_runoff = potential - possible;
 
-        soil_rechr_storage += possible;
+        DTO.soil_rechr_storage += possible;
 
-        if (soil_rechr_storage > soil_rechr_max)
-            _push_excess_down(soil_rechr_storage,soil_rechr_max,soil_lower_storage);
+        if (DTO.soil_rechr_storage > DTO.soil_rechr_max)
+            _push_excess_down(DTO.soil_rechr_storage,DTO.soil_rechr_max,soil_lower_storage);
 
-        soil_storage = soil_lower_storage + soil_rechr_storage;
+        DTO.soil_storage = soil_lower_storage + DTO.soil_rechr_storage;
 
-        if (soil_storage > soil_storage_max)
-            _push_excess_down(soil_storage,soil_storage_max,soil_excess_to_gw);
+        if (DTO.soil_storage > DTO.soil_storage_max)
+            _push_excess_down(DTO.soil_storage,DTO.soil_storage_max,DTO.soil_excess_to_gw);
         
-        if (_input.swe == 0.0) // if there is no snowcover
+        if (DTO.swe == 0.0) // if there is no snowcover
         {
-            rechr_to_ssr = soil_rechr_storage / soil_rechr_max * K_rechr_to_ssr * layer_thaw_fraction[0];
+            DTO.rechr_to_ssr = DTO.soil_rechr_storage / DTO.soil_rechr_max * DTO.K_rechr_to_ssr * DTO.thaw_fraction_rechr;
+            DTO.rechr_to_ssr = std::min(DTO.rechr_to_ssr,DTO.soil_rechr_storage * DTO.thaw_fraction_rechr);
 
-            if (rechr_to_ssr > soil_rechr_storage * layer_thaw_fraction[0])
-                rechr_to_ssr = soil_rechr_storage * layer_thaw_fraction[0];
+            DTO.soil_rechr_storage = std::max(0.0, DTO.soil_rechr_storage - DTO.rechr_to_ssr);
 
-            soil_rechr_storage = std::max(0.0, soil_rechr_storage - rechr_to_ssr);
-
-            soil_storage -= rechr_to_ssr;
-            soil_to_ssr = rechr_to_ssr;
+            DTO.soil_storage -= DTO.rechr_to_ssr;
+            DTO.soil_to_ssr = DTO.rechr_to_ssr;
 
         }
 
-        if (soil_excess_to_gw > K_soil_to_gw * layer_thaw_fraction[1])
+        if (DTO.soil_excess_to_gw > DTO.K_soil_to_gw * thaw_fraction_lower)
         {
-            double excess_to_gw_max = K_soil_to_gw * layer_thaw_fraction[1];
-            _push_excess_down(soil_excess_to_gw,excess_to_gw_max,excess);
+            double excess_to_gw_max = DTO.K_soil_to_gw * thaw_fraction_lower;
+            _push_excess_down(DTO.soil_excess_to_gw,excess_to_gw_max,DTO.excess);
         }
 
         // Line 607 of SoilX crhmcommetns branch, comment says upper layer but code says lower-layer, ask logan about this.
-        if (excess_to_ssr && excess > 0.0)
+        if (DTO.excess_to_ssr && DTO.excess > 0.0)
         {
-            double excess_to_ssr_max = excess * (1.0 -  layer_thaw_fraction[1]);
-            _push_excess_down(excess,excess_to_ssr_max,soil_to_ssr);
+            double excess_to_ssr_max = DTO.excess * (1.0 - thaw_fraction_lower);
+            _push_excess_down(DTO.excess,excess_to_ssr_max,DTO.soil_to_ssr);
         }
 
         
         
     else
-        excess = _input.infil + _output.condensation;
+        DTO.excess = DTO.infil + DTO.condensation;
 
     
 };
 
-void soil_two_layer::manage_detention(input_soil_two_layers& _input, output_soil_two_layer& _output,double& excess)
+void soil_two_layer::manage_detention()
 {
-    _output.soil_excess_to_runoff += _input.runoff + excess + routing_residual / face_area; // routing_residual comes from the crhm varaible redirected_residual which has units of mm*km^2/int (not sure why), so face_area is there for now for consistency.
-    // TODO is the above in _output?                                   
+    double face_area = 1.0; // Later will be pulled from face, but since routine_residual is always zero, ignoring it here.
+    DTO.soil_excess_to_runoff += DTO.runoff + DTO.excess + DTO.routing_residual / face_area; // routing_residual comes from the crhm varaible redirected_residual which has units of mm*km^2/int (not sure why), so face_area is there for now for consistency.
         
-    routing_residual = 0.0;
-
-    if (_output.soil_excess_to_runoff > 0.0)
+    
+    if (DTO.soil_excess_to_runoff > 0.0)
     {
-        if (_input.swe <= snow_covered_threshold)
-            detention_max = detention_snow_max;
+        double detention_max;
+        if (DTO.swe <= DTO.snow_covered_threshold)
+            detention_max = DTO.detention_snow_max;
         else
-            detention_max = detention_organic_max;
+            detention_max = DTO.detention_organic_max;
 
-        double detention_space = detention_max - detention_storage;
+        double detention_space = detention_max - DTO.detention_storage;
 
         if (detention_space > 0.0)
         {
-            if (_output.soil_excess_to_runoff > detention_space)
+            if (DTO.soil_excess_to_runoff > detention_space)
             {
-                _output.soil_excess_to_runoff = std::max(0.0,_output.soil_excess_to_runoff - detention_space); 
-                detention_storage += detention_space;
+                DTO.soil_excess_to_runoff = std::max(0.0,DTO.soil_excess_to_runoff - detention_space); 
+                DTO.detention_storage += detention_space;
             }
             else
             {
-                detention_storage += _output.soil_excess_to_runoff;
-                soil_excess_to_runoff = 0.0;
+                DTO.detention_storage += DTO.soil_excess_to_runoff;
+                DTO.soil_excess_to_runoff = 0.0;
             }
         }
     }
 
-    if (_input.swe <= snow_covered_threshold) // default will be zero
-        K_detention_to_runoff = K_detention_organic_to_runoff;
-    else
-        K_detention_to_runoff = K_detention_snow_to_runoff;
-
-    if (detention_storage > 0.0 && K_detention_to_runoff > 0.0)
+    if (DTO.detention_storage > 0.0)
     {
-        double transfer = std::min(detention_storage,K_detention_storage);
-        _output.soil_excess_to_runoff += transfer;
-        detention_storage -= transfer;
+        double K_detention_to_runoff;
+
+        if (DTO.swe <= DTO.snow_covered_threshold) // default will be zero
+            K_detention_to_runoff = DTO.K_detention_organic_to_runoff;
+        else
+            K_detention_to_runoff = DTO.K_detention_snow_to_runoff;
+
+        if (K_detention_to_runoff > 0.0)
+        {
+            double transfer = std::min(DTO.detention_storage,K_detention_to_runoff);
+            DTO.soil_excess_to_runoff += transfer;
+            DTO.detention_storage -= transfer;
+        }
+
+        if (DTO.detention_storage < 0.0001) // from CRHM, for safety and to drop any Floating-point errors
+            DTO.detention_storage = 0.0;
     }
-
-    if (detention_storage < 0.0001) // from CRHM, for safety and to drop any Floating-point errors
-        detention_storage = 0.0;
-
 };
 
-void soil_two_layer::manage_depression(input_soil_two_layers& _input, output_soil_two_layers& _output)
+void soil_two_layer::manage_depression()
 {
-    if (_output.soil_excess_to_runoff > 0.0 && depression_storage > 0.0)
+    if (DTO.soil_excess_to_runoff > 0.0 && DTO.depression_storage > 0.0)
     {
-        double exponent = -1.0 * std::min(12.0,_output.soil_excess_to_runoff / depression_max);
+        double exponent = -1.0 * std::min(12.0,DTO.soil_excess_to_runoff / DTO.depression_max);
 
-        double depression_space = (depression_max - depression_storage) * (1 - exp(exponent));        
+        double depression_space = (DTO.depression_max - DTO.depression_storage) * (1 - exp(exponent));        
 
-        if (soil_storage_max == 0.0)
-            depression_space = depression_max - depression_storage;
+        if (DTO.soil_storage_max == 0.0)
+            depression_space = DTO.depression_max - DTO.depression_storage;
 
         if (depression_space > 0.0)
         {
-            if (_output.soil_excess_to_runoff > depression_space)
+            if (DTO.soil_excess_to_runoff > depression_space)
             {
-                _output.soil_excess_to_runoff = std::max(0.0,_output_soil_excess_to_runoff - depression_space);
-                depression_storage += depression_space;
+                DTO.soil_excess_to_runoff = std::max(0.0,DTO.soil_excess_to_runoff - depression_space);
+                DTO.depression_storage += depression_space;
                 // TODO add total tracker
             }
             else
             {
-                depression_storage += _output.soil_excess_to_runoff;
+                DTO.depression_storage += DTO.soil_excess_to_runoff;
                 // TODO add total tracker
-                _output.soil_excess_to_runoff = 0.0;
+                DTO.soil_excess_to_runoff = 0.0;
             }
 
         }
     }
 
-    if (depression_storage > 0.0 && K_depression_to_gw > 0.0)
+    if (DTO.depression_storage > 0.0 && DTO.K_depression_to_gw > 0.0)
     {
-        double value = transfer_min(depression_storage,K_depression_to_gw);
-        soil_depresssion_to_gw += value;
-        depression_storage -= value;
-        if (depression_storage < 0.0) // floatig point error safety?
-            depression_storage = 0.0;
+        double value = transfer_min(DTO.depression_storage,DTO.K_depression_to_gw);
+        DTO.soil_depresssion_to_gw += value;
+        DTO.depression_storage -= value;
+        if (DTO.depression_storage < 0.0) // floatig point error safety?
+            DTO.depression_storage = 0.0;
     }
 
 
 };
 
-double soil_two_layer::transfer_min(double& val1, double& val2)
-{
-    return std::min(val1,val2);
-};
+
 
 void soil_two_layer::manage_groundwater()
 {
-    ground_water_storage += soil_depression_to_gw;
-    ground_water_flow = 0.0;
+    DTO.ground_water_storage += DTO.soil_depression_to_gw;
+    DTO.ground_water_flow = 0.0;
 
-    if (ground_water_storage > ground_water_max)
+    if (DTO.ground_water_storage > DTO.ground_water_max)
     {
-        _push_excess_down(ground_water_storage,ground_water_max,ground_water_flow);
+        _push_excess_down(DTO.ground_water_storage,DTO.ground_water_max,DTO.ground_water_flow);
     }
 
-    if (ground_water_max > 0.0) // divide by zero safety
+    if (DTO.ground_water_max > 0.0) // divide by zero safety
     {
-        double spilled = ground_water_storage / ground_water_max * K_ground_water_out;
-        ground_water_storage -= spill;
-        ground_water_flow += spill;
+        double spilled = DTO.ground_water_storage / DTO.ground_water_max * DTO.K_ground_water_out;
+        DTO.ground_water_storage -= spill;
+        DTO.ground_water_flow += spill;
     }
 
     // TODO daily and simulation totals, like in CRHM?
 
 };
 
-void soil_two_layer::manage_subsurface_runoff(input_soil_two_layers& _input, output_soil_two_layers& _output)
+void soil_two_layer::manage_subsurface_runoff()
 {
-    if (depression_storage > 0.0 && K_depression_to_ssr > 0.0)
+    if (DTO.depression_storage > 0.0 && DTO.K_depression_to_ssr > 0.0)
     {
-        double value = transfer_min(depression_storage,K_depression_to_ssr);
-        soil_to_ssr += value;
-        depression_storage -= value;
-        if (depression_storage < 0.0)
-            depression_storage = 0.0;
+        double value = transfer_min(DTO.depression_storage,DTO.K_depression_to_ssr);
+        DTO.soil_to_ssr += value;
+        DTO.depression_storage -= value;
+        if (DTO.depression_storage < 0.0)
+            DTO.depression_storage = 0.0;
     }
 
-    if (K_lower_to_ssr > 0.0)
+    if (DTO.K_lower_to_ssr > 0.0)
     {
-        double available = soil_storage - soil_rechr;
-        double value = transfer_min(K_lower_to_ssr * layer_thaw_fraction[1],available);
-        soil_storage -= value;
-        soil_to_ssr += value;
+        double available = DTO.soil_storage - DTO.soil_rechr;
+        double value = transfer_min(DTO.K_lower_to_ssr * thaw_fraction_lower,available);
+        DTO.soil_storage -= value;
+        DTO.soil_to_ssr += value;
     }
 
 };
-
-
-                
 
 
 void soil_two_layer::_push_excess_down(double& layer_storage, double& layer_max, double& layer_down)
@@ -245,52 +292,9 @@ void soil_two_layer::_push_excess_down(double& layer_storage, double& layer_max,
     layer_down += excess;
 }
     
-
-
-void soil_two_layer::set_layer_thaw_fraction()
+double soil_two_layer::transfer_min(double& val1, double& val2)
 {
-    std::fill(layer_thaw_fraction.begin(),layer_thaw_fraction.end(),0.0);
-
-
-    for (int layer = 0; layer < num_layers; ++layer)
-    {
-        if (thaw_front_depth == 0.0 && freeze_front_depth == 0.0)
-            layer_thaw_fraction[layer] = 1.0; 
-        else 
-        {
-            
-            layer_thaw_fraction[layer] = _find_thaw_fraction(layer);
-            if (layer_thaw_fraction[layer] < 1.0)
-                break;
-        }
-    }
-
-    
-};
-
-double soil_two_layers::_find_thaw_fraction(int& layer)
-{
-    double layer_start = 0.0 // mm
-    if (depth[layer] >= thaw_front_depth)
-    {
-        layer_start = std::accumulate(depth.begin(), depth.begin + layer + 1, 0.0);
-        return (thaw_front_depth - layer_start) / depth[layer];
-    else
-        return 1.0;
-    }
-};
-
-void soil_two_layers::set_condensation(double& ET, double& condense)
-{
-    if (ET < 0.0)
-    {
-        condense = -1.0 * ET;
-        ET = 0.0;
-    }
-};
-void soil_two_layer::set_data(soil_module::data& _d)
-{
-    d = &_d;
+    return std::min(val1,val2);
 };
 
 
--- src/modules/soil_submodules/soil_two_layer.hpp
+++ src/modules/soil_submodules/soil_two_layer.hpp
@@ -3,137 +3,36 @@
 #include "soil_base.hpp"
 #include "soil.hpp"
 #include <algorithm>
+#include "soil_DTO.hpp"
 
-
-class two_layer_DTO
-{
-public:
-    // note does not include actual ET except for main variables
-    // new each time step
-    // TODO this is memory inefficient, could store in/out variables as unique_ptr that is started when ready.
-    // TODO split soil modules into submodules (one for gw, one for settting up ssr, one for layers, etc.)
-    // TODO after splitting, make a separate DTO which is then combined into a single DTO (this one).
-    double thaw_front_depth; //in - XG
-    double freeze_front_depth; //in - XG
-    double potential_ET; //in - evapT
-    double swe; //in - snobal TODO should this include input from PBSM3D?
-    double infil; //in - infil_all
-    double runoff; //in - infil_all 
-    double routing_residual; //in - NetRoute
-
-    double condensation; //out (and used)
-    double actual_ET; //out
-    double soil_excess_to_runoff; //out (and used)
-    double soil_excess_to_gw; //out (and used)
-    double ground_water_outflow; //out (and used)
-    double soil_to_ssr; //out (and used)
-			// All K_ stuff are in
-    double K_soil_to_gw; // All these K's could be swapped to held from previous and could be global/system wide
-    double K_rechr_to_ssr;
-    double K_lower_to_ssr;
-    double K_detention_snow_to_runoff;
-    double K_detention_soil_to_runoff;
-    double K_depression_to_ssr;
-
-    // held from previous 
-    double soil_storage_max;
-    double soil_storage;
-    double soil_rechr_storage;
-    std::vector<double> layer_thaw_fraction; //2
-    double soil_rechr_max;
-    double soil_rechr_storage;
-    bool excess_to_ssr; 
-    double detention_max;
-    double detention_snow_max;
-    double detention_organic_max;
-    double detention_snow_init;
-    double detention_organic_init;
-    double depression_max;
-    double depression_storage;
-    double ground_water_storage;
-    double ground_water_max;
-    double ground_cover_type;
-    int soil_type;
-
-    // global constants
-    double num_layers;
-};
 // Temporary forward declaration
 // TODO add include after writing this
 class k_estimate;
 
-struct input_soil_two_layer : input_base
-{
-    double& thaw_front_depth;
-    double& freeze_front_depth;
-    double& potential_ET;
-    double& ET;    
-    double& swe;
-    double& infil;
-    double& runoff;
-};
-
-struct output_soil_two_layer : output_base
-{
-    double condensation = 0.0;
-    double actual_ET 0.0;
-    double soil_excess_to_runoff = 0.0;
-    double soil_excess_to_gw = 0.0;
-    double gw_flow = 0.0;
-    double soil_to_ssr = 0.0;
-}
-
 class soil_two_layer : public soil_base
 {
 public: // methods
-    soil_two_layer(int& _num_layers, soil_module::data& _d);
+    soil_two_layer(two_layer_DTO& _DTO);
     ~soil_two_layer();
 
-    void run(input_base& input, output_base& output) override;
+    void run() override;
 
 private: //methods
     
-    
-    int _find_thaw_front_layer(const std::vector<double>& depths, double& value);
-    void set_K_values();
-    void set_thaw_fraction(double value, int layer); // set the new value in a layer, ensure values are between 0 and 1.
-
-public: //members
-    // change if modified by other classes
-    double detention_storage;
-    double depression_storage;
-    double soil_storage;
-    double soil_rechr_storage;
-    double ground_water_storage;
-    const double routing_residual = 0.0;
-
-
-private: //members
-    int& num_layers;
-    double& soil_storage_max;
-    double& soil_rechr_max;
-    double& detention_storage_max;
-    double& detention_max;
-    double& detention_snow_max;
-    double& detention_soil_max;
-    double& depression_max;
-    double& detention_snow_init; // Might make these passed through the constructor jsut for initialization of detention storage
-    double& detention_soil_init; //same as above
-    double& ground_water_max;
-    double& ground_cover_type;
-    double& snow_covered_threshold;
-    int soil_type;
-    double K_depression_to_ssr;
-    double K_detention_snow_to_runoff;
-    double K_detention_soil_to_runoff;
-    double K_rechr_to_ssr;
-    double K_lower_to_ssr;
-    double K_soil_to_gw;
-    bool& excess_to_ssr; //global
-    
+    two_layer_DTO& DTO; 
     k_estimate k_estimator; 
     
-    std::vector<double> layer_thaw_fraction; // thaw_layers_lay
-    std::vector<double> 
+    
+    void set_K_values();
+    void set_layer_thaw_fraction(); // set the new value in a layer, ensure values are between 0 and 1.
+    void set_condensation();
+    void organize_soil_layers();
+    void manage_detention();
+    void manage_depresssion();
+    void manage_groundwater();
+    void manage_subsurface_runoff();
 
+
+    void _push_excess_down(double& layer_storage, double& layer_max, double& layer_down); 
+    double transfer_min(double& val1, double& val2);
 };
