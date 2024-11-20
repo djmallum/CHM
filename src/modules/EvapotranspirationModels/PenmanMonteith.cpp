c11fd41aa5dc6b31ba6713dcb9e33978e9ac70db
Allum <donovan.allum@usask.ca>
2024-11-20 13:36:40 -0700

    Modified LAI in PM ET submodule
    When I originally ported this module, I made the choice to avoid
    recalculating LAI which it exists already in CHM for simple canopy.
    However, to ensure a match with CRHM I've modified this part of the
    Penman-Monteith class where the LAI is computed based on a growth
    factor, vegetation height, and maximum and minimum LAI values.
    
    Luckily for this run the growth factor is equal to 1, to avoid
    additional changes, I set this as 1 instead of adding it as a new input.

--- src/modules/EvapotranspirationModels/PenmanMonteith.cpp
+++ src/modules/EvapotranspirationModels/PenmanMonteith.cpp
@@ -43,7 +43,9 @@ double PenmanMonteith::CalcStomatalResistance(const PM_vars& var)
     // In CRHM, the below calculation is an option, for now just use the minimum option.
     if (has_vegetation)
     {
-        rcstar = stomatal_resistance_min * leaf_area_index_max / leaf_area_index;
+        double LAI = veg_Ht/2.0*leaf_area_index_max; //TODO ad hoc for test
+        rcstar = stomatal_resistance_min * leaf_area_index_max / LAI;
+        // rcstar = stomatal_resistance_min * leaf_area_index_max / leaf_area_index; TODO commented for test
     }
     // TODO check units. for example, short_wave_in - 1.5 is suspect
     double f1 = 1.0;
