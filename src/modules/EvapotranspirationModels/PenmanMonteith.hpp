b0786a872ffb0c2f45535f93e89f492f962ec286
Allum <donovan.allum@usask.ca>
2025-01-23 16:11:51 -0700

    Added iostream for debugging

--- src/modules/EvapotranspirationModels/PenmanMonteith.hpp
+++ src/modules/EvapotranspirationModels/PenmanMonteith.hpp
@@ -2,7 +2,7 @@
 
 #include "evapbase.hpp"
 #include <algorithm>
-
+#include <iostream>
 // Implementation:
 // include the header
 // std::unique_ptr<evapT_base> mymodel = std::make_unqiue<PenmanMonteith>();
