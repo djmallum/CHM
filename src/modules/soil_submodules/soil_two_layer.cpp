33f2e399783a2a25368f7b37179b1cfff15b2b63
Allum <donovan.allum@usask.ca>
2025-02-18 16:53:23 -0700

    ground_water_out already initized at zero, removed redundant line

--- src/modules/soil_submodules/soil_two_layer.cpp
+++ src/modules/soil_submodules/soil_two_layer.cpp
@@ -239,7 +239,6 @@ void soil_two_layer::manage_depression()
 void soil_two_layer::manage_groundwater()
 {
     DTO.ground_water_storage += DTO.depression_to_gw; // TODO Possible error source, should be adding soil_excess_to_gw rather than depression to gw
-    DTO.ground_water_out = 0.0;
 
     if (DTO.ground_water_storage > DTO.ground_water_max)
     {
