c1d8354992e11c99f38c05558eef87df13b50595
Allum <donovan.allum@usask.ca>
2025-03-04 16:24:44 -0700

    Adjusted ET functions to have more sensible units
    Original equations for the latent heat (lambda) of vapourization and the
    psychrometric constant are reworked to have better units (Joules rather
    than kilijoules or megajoules) and more true to their original form.
    
    Such as the psychrometric constant, it depends on the heat capacity of
    air but the original equation does not.
    
    Arguments of these functions have been adjusted to be const as well.

--- src/modules/EvapotranspirationModels/PriestleyTaylor.cpp
+++ src/modules/EvapotranspirationModels/PriestleyTaylor.cpp
@@ -1,6 +1,6 @@
 #include "PriestleyTaylor.hpp"
 
-PriestleyTaylor::PriestleyTaylor(const double& alpha_const) : alpha(alpha_const)
+PriestleyTaylor::PriestleyTaylor(const double& alpha_const, const double& Cp) : alpha(alpha_const), heat_capacity_air(Cp)
 {
 
 }
@@ -16,5 +16,5 @@ void PriestleyTaylor::CalcEvapT(var_base& basevar, model_output& output)
 
     double Q = var.all_wave_net * (1 - Frac_to_ground);
 
-    output.ET = alpha * delta(var.air_temperature) * Q / (delta(var.air_temperature) + gamma(var.P_atm,var.air_temperature) );
+    output.ET = alpha * delta(var.air_temperature) * Q / (delta(var.air_temperature) + gamma(var.P_atm,var.air_temperature,heat_capacity_air) );
 }
