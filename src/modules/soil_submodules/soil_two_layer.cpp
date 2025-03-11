e771c4f0480f9db09a1196c968fd1530b590ee8a
Allum <donovan.allum@usask.ca>
2025-03-11 17:55:46 -0600

    Modified the soil_two_layer thaw fraction calculation
    These modifications make the code more like CRHM and likley corrects
    some errors. Unit tests couldn't find significant differences but
    keeping this anyway.

--- src/modules/soil_submodules/soil_two_layer.cpp
+++ src/modules/soil_submodules/soil_two_layer.cpp
@@ -66,18 +66,32 @@ void soil_two_layer::set_layer_thaw_fraction()
         DTO.thaw_fraction_rechr = 1.0;
         DTO.thaw_fraction_lower = 1.0;
     }
-    else 
+    else
+    {  
+        DTO.thaw_fraction_rechr = 0.0;
+        DTO.thaw_fraction_lower = 0.0;
+    }
+
+    if (DTO.soil_storage_max > 0.0 && (DTO.thaw_front_depth > 0.0 || DTO.freeze_front_depth > 0.0)) 
     {
+
         // TODO Verify this calculation
         if (DTO.thaw_front_depth < rechr_depth)
+        {
             DTO.thaw_fraction_rechr = DTO.thaw_front_depth / rechr_depth;
-        
-        else
+        }
+        else if ( DTO.thaw_front_depth < soil_depth)
         {
             DTO.thaw_fraction_rechr = 1.0;
             DTO.thaw_fraction_lower = (DTO.thaw_front_depth - rechr_depth) 
                 / (soil_depth - rechr_depth);
-        }   
+        }
+        else 
+        {
+            DTO.thaw_fraction_rechr = 1.0;
+            DTO.thaw_fraction_lower = 1.0;        
+        }
+           
     }
     
 };
