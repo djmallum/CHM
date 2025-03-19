0e54f57a6cd747fc2f0e6f298cd2d57d4525d486
Allum <donovan.allum@usask.ca>
2025-03-19 15:18:09 -0600

    Modified CHM code for unit tests
    Unit tests compare CHM output of Evapotranspiration_All, soil_module and
    Infil_All to CRHM output. Inputs to these modules are supplied by
    point_mode after these adjustments.
    
    core.cpp changes are done so that the code is able to properly find the
    triangle. This is a unit test where it is based on a by-hand MESHER
    output. CHM requires that you pinpoint a specific point on the MESH
    domain and so if your triangle is 'wrong' it will fail.
    
    CRHM runs K_Estimate *after* the SoilX module. Therefore, here, we move
    The K_Estimate calculation in soil_two_layer to after the soil moisture
    movement.
    
    Note that it is still not after soil_ET and should be considered in
    these tests.

--- src/modules/soil_submodules/soil_two_layer.cpp
+++ src/modules/soil_submodules/soil_two_layer.cpp
@@ -5,9 +5,6 @@ void soil_two_layer::run()
 
     DTO.routing_residual = 0.0; // TODO zero'd because routing doesn't exist yet.
                                 // also would need to remove or use face_area
-
-    k_estimator.run();
-
     initialize_single_step_vars(); 
 
     //set_K_values();
@@ -26,6 +23,7 @@ void soil_two_layer::run()
 
     manage_subsurface_runoff();
     
+    k_estimator.run();
 };
 
 void soil_two_layer::initialize_single_step_vars()
