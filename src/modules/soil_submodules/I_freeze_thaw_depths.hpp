497b4ffb594ba6b3ed86dcaad92f4a09fbcd605b
Allum <donovan.allum@usask.ca>
2025-04-04 16:57:33 -0600

    Added some variables and functions to baseclass

--- src/modules/soil_submodules/I_freeze_thaw_depths.hpp
+++ src/modules/soil_submodules/I_freeze_thaw_depths.hpp
@@ -4,4 +4,11 @@ class I_freeze_thaw_depths
 public:
     virtual ~I_freeze_thaw_depths() = default;
     virtual void run(void) = 0;
+
+    double thaw_front_depth = 0.0;
+    double freeze_front_depth = 0.0;
+
+    double get_freeze_front() { return freeze_front_depth;};
+
+    double get_thaw_depth() { return thaw_front_depth; };
 };
