0e68941ba649f78022151fa438b44f1af05d42e1
Allum <donovan.allum@usask.ca>
2025-04-07 16:45:20 -0600

    XG_algorithm Update for submodule standards
    Simplest implementation for TDD
    
    Stateless (mostly).
    
    Initialized on each step with objects stored by the module between steps
    (state and param classes).

--- src/modules/soil_submodules/XG_algorithm.cpp
+++ src/modules/soil_submodules/XG_algorithm.cpp
@@ -1,102 +1,358 @@
 #include "XG_algorithm.hpp"
 
+
 void XG_algorithm::run()
 {
+    double L = 335000;
+    
+    if (P.is_newday)
+       S.B = 0.0;
 
-    if (DTO.is_day_start(DTO))
-        B = 0.0;
+    S.B += surface_temp / P.time_step_per_day;
 
-    B += DTO.t_surface;
+    S.TrigAcc += B;
 
-    if (STO.is_day_start(DTO))
+    S.t_trend -= S.t_trend / 192;
+    S.t_trend += B/192;
+
+    // ensure that Zd_front is the right size here
+
+    if (P.is_newday)
     {
-        // idle to freeze/thaw or freeze/thaw to idle
-        
-        // Keep T_total within +/- T_threshold
-        // TODO might not be needed, might be diagnositic like B
-        if (T_total > T_threshold)
-            T_total = T_threshold;
-        else if (T_total < -T_threshold)
-            T_total = -T_threshold;
+        if (S.TrigAcc > P.Trigthrhld)
+            S.TrigAcc = P.Trigthrhld;
+        else if (S.TrigAcc < -P.Trigthrhld)
+            S.TrigAcc = -P.Trigthrhold;
 
-        if (std::abs(T_total) >= T_threshold/2.0 && freeze_thaw_state == 0)
+        if (S.TrigAcc >= P.Trigthrhld / 2.0 && S.TrigState == 0 && (S.Zdf > 0.0 || nfront > 0))
         {
-            if (freeze_front_depth < 0.0 || num_fronts)
-                freeze_thaw_state = 1;
+            S.TrigState = 1;
+            S.Zd_front[1] = -S.Zdf;
+            S.t_trend = 0.0;
+        }            
+
+        if (S.TrigAcc <= -P.Trigthrhld / 2.0 && S.TrigState == 0)
+        {
+            S.TrigState = -1;
+            S.Zd_front[1] = S.Zdt;
+            S.t_trend = 0.0;
+        }
+
+        if (S.TrigState == -1 && S.TrigAcc >= P.Trigthrhld/2.0 && S.t_trend > 0.0)
+        {
+            S.TrigState = 0;
+
+            if (S.Zdt > 0.0 && S.Zdf > 0.0)
+            {
+                if (S.Zdt > S.Zdf)
+                {
+                    push_front(S.Zdt);
+                    S.Zdt = 0.0;
+                    S.Bth = 0.0;
+                    S.Zd_front[0] = 0.0;
+                    S.Zd_front[1] = -S.Zdf;
+                }
+            }
+        }
+
+        if (S.TrigState == 1 && S.TrigAcc <= -P.Trigthrhld / 2.0 && S.t_trend < 0.0)
+        {
+            S.TrigState = 0;
+
+            if (S.Zdf > 0.0 && S.Zdt > 0.0)
+            {
+                if (S.Zdf > S.Zdt)
+                {
+                    push_front(-S.Zdf);
+                    S.Zdf = 0.0;
+                    S.Bfr = 0.0;
+                    S.Zd_front[0] = 0.0;
+                    S.Zd_front[1] = S.Zdt;
+                }
+            }
+        }
+
+        // Calculate Thermal Conductivities
+
+        S.Th_low = 1;
+        S.Fz_low = 1;
+        S.Check_XG_moist = 0.0;
+
+        for (int layer = 0; layer < S.Zd_front.size(); ++layer)
+        {
+            if (P.soil_moist_max > 0.0)
+                S.XG_moist[layer] = S.rechr_fract[layer] * S.XG_max[layer] * P.soil_rechr / P.soil_rechr_max
+                    + S.moist_fract[layer] * S.XG_max[layer] * P.soil_lower / P.soil_lower_max;
             else
-                freeze_thaw_state = -1;
+                S.XG_moist[layer] = 0.0;
 
-            temp_trend = 0.0;
-        }
+            S.check_XG_moist += S.XG_moist[layer];
 
-        if (freeze_thaw_state && std::abs(T_total) >= T_threshold/2.0 && std::abs(temp_trend) > 0.0)
-        {
-            freeze_thaw_state = 0;
+            S.XG_moist[layer] += S.default_fract[layer] * S.XG_max[layer] * P.theta_default[layer];
 
-            if (freeze_front_depth > 0.0 && thaw_front_depth > 0.0)
-            {
-                if ( is_freezing() )
-                    switch_to_idle(thaw_front_depth);
-                else
-                    switch_to_idle(-freeze_front_depth);
-            }
-        }
+            S.theta[layer] = S.XG_moist[layer] / S.XG_max[layer];
 
-        // Calculate thermal conductivities
+            if (S.theta[layer] < P.theta_min)
+                S.theta[layer] = P.theta_min;
 
-        // Issues 
-
-        thaw_low_layer = 1;
-        freeze_low_layer = 1; 
-         
-        double thaw_k_T_rechr = get_k_T(DTO.soil_rechr_storage);
-        double freeze_k_T_rechr = ;
-        double thaw_k_T_lower = ;
-        double freeze_k_T_lower = ;
-         
-        for (int ii = 0; ii < N_layers; ++ii)
-        {
-            if (DTO.soil_storage_max > 0.0)
-            {
-                 
-            }
-
-        }    
-    }
-
-
-
-
-
-
+            S.h2o[layer] = S.theta[layer] * S.por[layer] * 1000;
             
+            if(P.k_update)
+            { // change all layers to dynamic
+                if(S.ftc_contents[layer] == 1) // always set somewhere else
+                  S.ftc[layer] = get_ttc(layer);
+                else
+                  S.ftc[layer] = get_ftc(layer);
+
+                if(ttc_contents[layer] == 1) // always set somewhere else
+                  S.ttc[layer] = get_ftc(layer);
+                else
+                  S.ttc[layer] = get_ttc(layer);
+            }
+            else
+            {
+                S.ftc[layer] = get_ftc(layer);
+                S.ttc[layer] = get_ttc(layer);
+
+                S.ftc_contents[layer] = 0;
+                S.ttc_contents[layer] = 0;
+            }
+        }
+    
+        for (long layer = 1; layer < S.Zd_front.size(); ++layer) 
+        {
+            S.pf[layer] = std::sqrt(P.ftc[layer-1]*S.h2o[layer]/(P.ftc[layer]*S.h2o[layer-1])); // water kg/m3
+            S.pt[layer] = std::sqrt(P.ttc[layer-1]*S.h2o[layer]/(P.ttc[layer]*S.h2o[layer-1])); // water kg/m3
+        }
+
+        if(S.TrigState < 0.0) // handle freezing
+        {
+            S.Bfr -= S.B;  // Calculate the value of Degree-day for every day
+            
+            if(S.Bfr > 0.0)
+            {
+                freeze(); // XG-Algorithm - Freezing
+                
+                // check for thaw front
+                if(S.Zdt > 0.0 && S.Zdf >= S.Zdt)
+                {
+                    if(S.nfront > 0)
+                    {
+                        double Last = last_front();
+                        
+                        if(Last < 0.0) // frozen front
+                        {
+                            S.Zdf = pop_front();
+                            find_freeze_D(S.Zdf);
+                            double Last = last_front();
+                            
+                            if(Last > 0.0) // thaw front
+                            {
+                                S.Zdt = pop_front();
+                                find_thaw_D(S.Zdf);
+                                S.Zd_front[1] = S.Zdt;
+                            }
+                            else if(Last < 0.0) // never two frozen fronts together
+                            {
+                               //CRHM has thos throw an error, never two frozen fronts 
+                            }
+                            else // no thaw front
+                            {
+                                S.Zdt = 0.0;
+                                S.Bth = 0.0;
+                                S.Zd_front[1] = 0.0;
+                            }
+                        }
+                        else if(Last < 0.0) // never two freeze fronts together
+                        {
+                            // CRHM had another throw here, but I dont think its possible to reach this ever.
+                        }
+                        else // no thaw layer
+                        {
+                            S.Zdt = 0.0;
+                            S.Bth = 0.0;
+                            S.Zd_front[1] = 0.0;
+                        }
+                    }
+                    else // no fronts
+                    {
+                        S.Zdt = 0.0;
+                        S.Bth = 0.0;
+                        S.Zd_front[1] = 0.0;
+                    }
+                }
+            }
+            S.Zd_front[0] = -S.Zdf;
+        } // freezing handled
+        else if(S.TrigState > 0) // Surface thawing lower ground frozen
+        {
+            S.Bth += S.B;  // Accumulate thawing degree-days
+
+            if(S.Bth <= 0.0)
+            {
+                S.Bfr = S.Bth;
+                S.Zdt = 0.0;
+                S.Bth = 0.0;
+            }
+            else
+            {
+                thaw(); // XG-Algorithm - Thawing
+                
+                // check for freeze front
+                if(S.Zdf > 0.0 && S.Zdt >= S.Zdf)
+                {
+                    if(S.nfront > 0)
+                    {
+                        double Last = last_front();
+                        
+                        if(Last > 0.0) // thaw front
+                        {
+                            S.Zdt = pop_front();
+                            find_thaw_D(S.Zdt);
+                            Last = last_front();
+                            
+                            if(Last < 0.0) // frozen front
+                            {
+                                S.Zdf = pop_front();
+                                find_freeze_D(S.Zdf);
+                                S.Zd_front[1] = -S.Zdf;
+                            }
+                            else if(Last > 0.0) // never two thaw fronts together
+                            {
+                                //CRHM throws exception here
+                            }
+                            else // no frozen front
+                            {
+                                S.Zdf = 0.0; // no frozen layers
+                                S.Bfr = 0.0;
+                                S.Zd_front[1] = 0.0;
+                            }
+                        }
+                        else if(Last < 0.0) // never two freeze fronts together
+                        {
+                            //Again, another throw that cannot be accessed, can remove this i think                        
+                        }
+                        else // no thaw layer
+                        {
+                            S.Zdf = 0.0;
+                            S.Bfr = 0.0;
+                            S.Zd_front[1] = 0.0;
+                        }
+                    }
+                    else // no fronts
+                    {
+                        S.Zdf = 0.0; // no frozen layer
+                        S.Bfr = 0.0;
+                        S.Zdt = 0.0;
+                        S.Bth = 0.0;
+                        S.TrigState = 0;
+                        S.Zd_front[1] = 0.0;
+                    }
+                } // if check freeze front
+            } // else thaw processing
+            
+            S.Zd_front[0] = S.Zdt;
+        } // thawing handled
+    }
 };
 
-bool XG_algorithm::is_freezing()
-{
-    if (freeze_thaw_state == -1 && T_total > 0.0 && temp_trend > 0.0)
-        return true;
-    else if (freeze_thaw_state == 1 && T_total < 0.0 && temp_trend < 0.0)
-        return false;
-}
-
-void XG_algorithm::switch_to_idle(double front_depth)
-{
-    push_front(front_depth); // TODO write this function
-    // TODO in crhm both parts modify Zd_front_array (something I've decided not to include in XG) come back if necessary
-    if (front_depth > 0.0 && thaw_front_depth > freeze_front_depth)
-    {
-        thaw_front_depth = 0.0;
-        thaw_degree_days = 0.0;
-    }
-    else (front_depth < 0.0 && freeze_front_depth > thaw_front_depth)
-    {
-        freeze_front_depth = 0.0;
-        freeze_degree_days = 0.0;
-    }
-
-};
-
-
-
-
+//void XG_algorithm::run()
+//{
+//
+//    if (DTO.is_day_start(DTO))
+//        B = 0.0;
+//
+//    B += DTO.t_surface;
+//
+//    if (STO.is_day_start(DTO))
+//    {
+//        // idle to freeze/thaw or freeze/thaw to idle
+//        
+//        // Keep T_total within +/- T_threshold
+//        // TODO might not be needed, might be diagnositic like B
+//        if (T_total > T_threshold)
+//            T_total = T_threshold;
+//        else if (T_total < -T_threshold)
+//            T_total = -T_threshold;
+//
+//        if (std::abs(T_total) >= T_threshold/2.0 && freeze_thaw_state == 0)
+//        {
+//            if (freeze_front_depth < 0.0 || num_fronts)
+//                freeze_thaw_state = 1;
+//            else
+//                freeze_thaw_state = -1;
+//
+//            temp_trend = 0.0;
+//        }
+//
+//        if (freeze_thaw_state && std::abs(T_total) >= T_threshold/2.0 && std::abs(temp_trend) > 0.0)
+//        {
+//            freeze_thaw_state = 0;
+//
+//            if (freeze_front_depth > 0.0 && thaw_front_depth > 0.0)
+//            {
+//                if ( is_freezing() )
+//                    switch_to_idle(thaw_front_depth);
+//                else
+//                    switch_to_idle(-freeze_front_depth);
+//            }
+//        }
+//
+//        // Calculate thermal conductivities
+//
+//        // Issues 
+//
+//        thaw_low_layer = 1;
+//        freeze_low_layer = 1; 
+//         
+//        double thaw_k_T_rechr = get_k_T(DTO.soil_rechr_storage);
+//        double freeze_k_T_rechr = ;
+//        double thaw_k_T_lower = ;
+//        double freeze_k_T_lower = ;
+//         
+//        for (int ii = 0; ii < N_layers; ++ii)
+//        {
+//            if (DTO.soil_storage_max > 0.0)
+//            {
+//                 
+//            }
+//
+//        }    
+//    }
+//
+//
+//
+//
+//
+//
+//            
+//};
+//
+//bool XG_algorithm::is_freezing()
+//{
+//    if (freeze_thaw_state == -1 && T_total > 0.0 && temp_trend > 0.0)
+//        return true;
+//    else if (freeze_thaw_state == 1 && T_total < 0.0 && temp_trend < 0.0)
+//        return false;
+//}
+//
+//void XG_algorithm::switch_to_idle(double front_depth)
+//{
+//    push_front(front_depth);// TODO write this function
+//    // TODO in crhm both parts modify Zd_front_array (something I've decided not to include in XG) come back if necessary
+//    if (front_depth > 0.0 && thaw_front_depth > freeze_front_depth)
+//    {
+//        thaw_front_depth = 0.0;
+//        thaw_degree_days = 0.0;
+//    }
+//    else (front_depth < 0.0 && freeze_front_depth > thaw_front_depth)
+//    {
+//        freeze_front_depth = 0.0;
+//        freeze_degree_days = 0.0;
+//    }
+//
+//};
+//
+//
+//
+//
--- src/modules/soil_submodules/XG_algorithm.hpp
+++ src/modules/soil_submodules/XG_algorithm.hpp
@@ -4,9 +4,98 @@
 class XG_algorithm : public I_freeze_thaw_depths
 {
 public:
-    ~XG_algorithm() {};
+    ~XG_algorithm(double& t, double& _SWE, state& _S, param& _P) : surface_temp(t), SWE(_SWE), S(_S), P(_P) {};
     XG_algorithm() {};
 
     virtual void run() override;
 
+    double& surface_temp;
+    double& SWE;
+    class state;
+    class param;
+    state& S;
+    param& P;
+    
+    class state
+    {
+    public:
+        // Depth-related
+        double Zdf;                // (m) depth of freezing front  
+        double Zdt;                // (m) depth of thawing front  
+        std::vector<double> Zd_front; // (m) depths of all freezing/thawing fronts (thaw: +, freeze: -)  
+        int Th_low;            // lowest thawed layer  
+        int Fz_low;            // lowest frozen layer  
+        int nfront;                // number of freezing/thawing fronts  
+
+        // Degree-day counters  
+        double Bfr;                // (ºC*d) freeze degree days  
+        double Bth;                // (ºC*d) thaw degree days  
+
+        // Layer ratios  
+        std::vector<double> pf;    // soil layers freezing ratios  
+        std::vector<double> pt;    // soil layers thawing ratios  
+
+        // Thermal properties  
+        std::vector<double> ttc;   // (W/(m*K)) thawing thermal conductivity  
+        std::vector<double> ftc;   // (W/(m*K)) freezing thermal conductivity  
+        std::vector<double> tc_composite;  // (W/(m*K)) composite ftc/ttc  
+        std::vector<double> tc_composite2; // (W/(m*K)) composite2 ftc/ttc  
+
+        // Moisture/water content  
+        std::vector<double> theta;           // (m³/m³) layer theta  
+        std::vector<double> layer_h2o;       // (kg/m³) layer water content  
+        double XG_moist_d;      // (m) layer depth of soil moisture in XG  
+        double XG_rechr_d;      // (m) layer depth of soil recharge in XG  
+        std::vector<double> XG_max;          // (mm) layer max soil moisture  
+        std::vector<double> XG_moist;        // (mm) layer moisture content  
+        double check_XG_moist;     // (mm) sum of XG soil_moist  
+
+        // Local/temporary  
+        double B;                  // (ºC*d) interval degree-day sum  
+        double TrigAcc;            // (ºC*d) freeze/thaw cycle detection  
+        int TrigState;             // 1/0/-1 → thaw/idle/freeze  
+        std::vector<int> ttc_contents;          // 0/1 → thaw/freeze  
+        std::vector<int> ftc_contents;          // 0/1 → freeze/thaw  
+        double t_trend;            // (°C) temperature long-term trend  
+
+        // Fractions  
+        std::vector<double> rechr_fract;   // fraction of layer (soil_rechr_max)  
+        std::vector<double> moist_fract;   // fraction of layer (soil_moist_max)  
+        std::vector<double> default_fract; // fraction of layer (theta_default)  
+
+    // Variation #1 only  
+    //int n_factor_T;            // days after start of thaw  
+    //double n_factor;           // calculated n_factor value
+    };
+
+    class params
+    {
+    public:
+    // Core parameters  
+        const double Trigthrhld;           // (ºC*d) Trigger reference level  
+        const std::vector<double> depths;  // (m) soil layer thicknesses  
+        const std::vector<double> por;     // soil porosity  
+        const int N_Soil_layers;           // number of soil layers (≤ nlay)  
+        const double theta_default;        // (m³/m³) default theta  
+        const double theta_min;            // (m³/m³) minimum theta  
+        const std::vector<double> soil_solid_km;        // (W/(m*K)) dry soil conductivity  
+        const std::vector<double> soil_solid_km_ki;     // (W/(m*K)) saturated frozen conductivity  
+        const std::vector<double> soil_solid_km_kw;     // (W/(m*K)) saturated unfrozen conductivity  
+        const double SWE_k;                // (W/(m*K)) snow thermal conductivity UNUSED IN CRHM 
+        //Next 3 are initial conditions
+        //const double Zdf_init;             // (m) initial freezing front depth  
+        //const double Zdt_init;             // (m) initial thawing front depth  
+        //const double Zpf_init;             // (m) initial permafrost depth  
+        const bool freeze_kw_ki_update;     // update kw→ki behind freeze front  
+        const bool thaw_ki_kw_update;       // update ki→kw behind thaw front  
+        const int k_update;                // 0=never, 1=post-layer, 2=continuous  
+        const double soil_rechr_max;       // (mm) max recharge zone capacity  
+        const double soil_moist_max;       // (mm) max rooting zone capacity  
+        const bool is_newday;
+        const double time_step_per_day;
+    };
+    // Variation #1 parameters  
+    //const double& n_factor_a;           // surface-to-air temp ratio  
+    //const double& n_factor_b;           // surface-to-air temp ratio  
+    //const double& n_factor_c;           // surface-to-air temp ratio  
 };
