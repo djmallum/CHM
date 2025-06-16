6bb71349d2f5f2237f9e7278a3abfa9c277edd42
Donovan Allum <donovan.allum@usask.ca>
2025-06-16 17:28:15 -0600

    Other Tests, soil and t_surface

---
+++ src/tests/submoduletests/test_soil_ET.cpp
@@ -0,0 +1,14 @@
+#include <gmock/gmock.h>
+#include "soil_DTO.hpp"
+#include <gtest/gtest.h>
+#define diff 0.0001
+
+class SoilETTest : public ::testing::Test
+{
+protected:
+	void SetUp override
+	{
+
+	};
+};
+
