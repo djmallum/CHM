900fdee418183db94d0936660d1a9ce237f7abbe
Allum <donovan.allum@usask.ca>
2025-06-24 17:55:00 -0600

    Updated surface_temperature to match cache_handler

--- src/modules/surface_temperature.cpp
+++ src/modules/surface_temperature.cpp
@@ -1,49 +1,107 @@
+#include "surface_temperature.hpp"
+
+REGISTER_MODULE_CPP(surface_temperature);
+
+surface_temperature::surface_temperature(config_file cfg) : module_base("surface_temperature", parallel::data,cfg)
+{
+    depends("swe");
+    depends("air_temperature");
+    depends("snow_depth");
+    depends("ground_heat_flux");
+    depends("snow_density");
+
+    provides("surface_temperature");
+    provides("snow_thermal_conductivity");
+    
+};
 void surface_temperature::init(mesh& domain)
 {
 	// enforces all access to data via cache is through face rather than the cache.
 	// automatically ends this option when it goes out of scope
-	auto guard = cache_handler<TempCache>::scoped_init();
+	auto guard = init_phase();
+
+    for (size_t i = 0; i < domain->size_faces(); i++)
+    {
+        auto face = domain->face(i);
+        auto& d = face->make_module_data<surface_temperature::data>(ID);
+
+        d.RCC = std::make_unique<RCC_surface_temperature<surface_temperature::API>>(d.api);
+
+        d.luce_tarboton = std::make_unique<luce_tarboton_surface_temperature<surface_temperature::API>>(d.api);
+        
+        d.api.set_face(face);
+        d.api.set_global(global_param_);
+    };
 };
 
-double& surface_temperature::data::air_temperature()
+void surface_temperature::run(mesh_elem& face)
 {
-	return Cache.get_field(
+    auto& d = face->make_module_data<surface_temperature::data>(ID);
+
+    double swe = (*face)["swe"_s];
+
+    if (swe == 0.0)
+        d.RCC->execute();
+    else
+        d.luce_tarboton->execute();
+
+    d.api.set_outputs_to_face();
+};
+
+double& surface_temperature::API::air_temperature()
+{
+	return Cache_.get_field(
 			[this]() -> double& { return (*face)["air_temperature"_s]; },
-			[](TempCache& c) -> double& { return c.air_temperature; }
-			)
+			[this]() -> double& { return Cache_.air_temperature; }
+			);
 };
 
-double& surface_temperature::data::snow_depth()
+double& surface_temperature::API::snow_depth()
 {
-	return Cache.get_field(
+	return Cache_.get_field(
 			[this]() -> double& { return (*face)["snow_depth"_s]; },
-			[](TempCache& c) -> double& { return c.snow_depth; }
-			)
+			[this]() -> double& { return Cache_.snow_depth; }
+			);
 };
 
-double& surface_temperature::data::ground_heat_flux()
+double& surface_temperature::API::ground_heat_flux()
 {
-	return Cache.get_field(
+	return Cache_.get_field(
 			[this]() -> double& { return (*face)["ground_heat_flux"_s]; },
-			[](TempCache& c) -> double& { return c.ground_heat_flux; }
-			)
+			[this]() -> double& { return Cache_.ground_heat_flux; }
+			);
 };
 
-void surface_temperature::data::surface_temperature(double& value)
+void surface_temperature::API::surface_temperature(const double& value)
 {
-	Cache.set_field(
-			[](TempCache& c) -> double& { return c.surface_temperature; }, value);
+	Cache_.set_field(
+			[this]() -> double& { return Cache_.surface_temperature; }, value);
 };
 
-void surface_temperature::data::snow_thermal_conductivity(double& value)
+void surface_temperature::API::snow_thermal_conductivity(const double& value)
 {
-	Cache.set_field(
-			[](TempCache& c) -> double& { return c.snow_thermal_conductivity; }, value);
+	Cache_.set_field(
+			[this]() -> double& { return Cache_.snow_thermal_conductivity; }, value);
 };
 
-void surface_temperature::data::set_outputs_to_face(TempCache&& c)
+void surface_temperature::API::set_outputs_to_face()
 {
+    TempCache& c = Cache_.get_cache();
 	(*face)["surface_temperature"_s] = c.surface_temperature;
 	(*face)["snow_thermal_conductivity"_s] = c.snow_thermal_conductivity;
 	c.reset();
 };
+
+bool surface_temperature::API::is_new_day()
+{
+    int td = global_param->posix_time().time_of_day().total_seconds();
+    int time_to_midnight = 86400 - td;
+    if (td >= 0 && td < global_param->dt()) //(time_to_midnight >= global_param->dt())
+    {
+        return true;
+    }
+    else
+        return false;
+};
+
+
--- src/modules/surface_temperature.hpp
+++ src/modules/surface_temperature.hpp
@@ -3,8 +3,10 @@
 #include "logger.hpp"
 #include "triangulation.hpp"
 #include "module_base.hpp"
-#include "surface_temperature_submodules/RCC_surface_temperature.hpp"
-#include "surface_temperature_submodules/luce_tarboton_surface_temperature.hpp"
+#include "RCC_surface_temperature.hpp"
+#include "luce_tarboton_surface_temperature.hpp"
+#include "cache_handler.hpp"
+#include <memory>
 
 class surface_temperature : public module_base
 {
@@ -16,32 +18,65 @@ public:
 	void init(mesh& domain);
 	void run(mesh_elem& face);
 
+    class API
+    {
+        mesh_elem face = nullptr;
+        global* global_param = nullptr;
+
+    public:
+        struct TempCache
+        {
+            double air_temperature = 0.0;
+            double thaw_front_depth = 0.0;
+            double snow_depth = 0.0;
+            double snow_density = 0.0;
+            double ground_heat_flux = 0.0;
+            double net_radiation = 0.0;
+
+            double surface_temperature = 0.0;
+            double snow_thermal_conductivity = 0.0;
+        };
+    private:
+		cache_handler<TempCache> Cache_;
+    
+    public:
+        //inputs
+        double& air_temperature();
+	    double& thaw_front_depth();
+		double& snow_depth();
+	    double& snow_density();
+        double& ground_heat_flux();
+	    double& net_radiation();
+
+        //outputs
+		void surface_temperature(const double& in);
+		void snow_thermal_conductivity(const double& in); // just if snow-covered
+        
+        // used by module    
+        void set_outputs_to_face();   
+        void set_face(mesh_elem& face_in)
+        { face = *face_in; };
+        void set_global(global* global_param_)
+        { global_param = global_param_; };
+
+        // daily accumulator
+        bool is_new_day();
+        double steps_per_day();
+    };
+
 	class data : public face_info
 	{
 	public:
+        std::unique_ptr<RCC_surface_temperature<API>> RCC;
+        std::unique_ptr<luce_tarboton_surface_temperature<API>> luce_tarboton;
 
-		double air_temperature();
-		
-		double snow_depth();
-		double ground_heat_flux();
-		
-		void surface_temperature(const double& in);
-		void snow_thermal_conductivity(const double& in);
-		
-	private:
-		mesh_elem* face = nullptr;
-		cache_handler<TempCache> Cache;
-
-		struct TempCache
-		{
-			double air_temperature = 0.0;
-			double snow_depth = 0.0;
-			double snow_density = 0.0;
-			double ground_heat_flux = 0.0;
-			double surface_temperature = 0.0;
-			double snow_thermal_conductivity = 0.0;
-		};
+        API api;
 	};
+
+    auto init_phase()
+    {
+        return cache_handler<API::TempCache>::scoped_init(); 
+    };
 
 };
 
