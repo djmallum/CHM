8fffb84ce415c4e47461ef08f39ce58e51f5a0ee
Donovan Allum <donovan.allum@usask.ca>
2025-06-24 00:46:00 -0600

    Completed cache_handler with a setter and getter and an initialization guard

--- src/modules/surface_temperature_submodules/cache_handler.hpp
+++ src/modules/surface_temperature_submodules/cache_handler.hpp
@@ -4,27 +4,48 @@
 template <typename CacheType>
 class cache_handler
 {
-    std::unique_ptr<CacheType> cache_;
-    static bool initializing_ = false;
+	std::unique_ptr<CacheType> cache_;
+    static inline bool initializing_ = false;
 public:
+
+	class initializer_guard
+	{
+	public:
+		initializer_guard() { initializing_ = true; };
+		~initializer_guard() { initializing_ = false; };
+	};
+
     template<typename Fetch, typename Access>
     auto& get_field(Fetch&& fetch, Access&& access)
     {
         if (initializing_)
             return fetch();
 
-        if (!cache)
+        if (!cache_)
         {
-            cache_ = std::make_unique<CacheType>();
-            access(cache_) = fetch();
+			cache_ = std::make_unique<CacheType>();
+			access(*cache_) = fetch();
         }
 
-        return access(cache_);
+        return access(*cache_);
 
     }
 
-    void reset() { cache.reset() };
-    static void start_init() { initialization = true; };
-    static void end_init() { initialization = false; };
+	template<typename Access>
+	void set_field(Access&& access, double& value)
+	{
+		if (!cache_)
+		{
+			cache_ = std::make_unique<CacheType>();
+		};
+	
+		access(*cache_) = value;
+	};
 
+    void reset() { cache_.reset(); };
+
+	CacheType& get_cache()
+	{ return *cache_; };
+
+	statis auto scoped_init() { return initializer_guard(); };
 };
