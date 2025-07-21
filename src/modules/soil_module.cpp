c1cdfb1716745022bd6f36daf24d4f32ee735c23
Allum <donovan.allum@usask.ca>
2025-07-21 16:16:48 -0600

    Replacing size_faces() with size_local_faces() to be consistent with f9b6557

--- src/modules/soil_module.cpp
+++ src/modules/soil_module.cpp
@@ -48,7 +48,7 @@ void soil_module::init(mesh& domain)
 
     SoilDataObj = std::make_unique<Soil::soils_na>();
 
-    for (size_t i = 0; i < domain->size_faces(); i++)
+    for (size_t i = 0; i < domain->size_local_faces(); i++)
     {
         auto face = domain->face(i);
         auto& d = face->make_module_data<soil_module::data>(ID);
