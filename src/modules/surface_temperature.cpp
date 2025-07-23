d049adde0ab1a71f23d1d6c47bc4896ecee52385
Allum <donovan.allum@usask.ca>
2025-07-23 17:42:13 -0600

    Updating surface_temperature module so submodules use single-instance base class and data uses data_base

--- src/modules/surface_temperature.cpp
+++ src/modules/surface_temperature.cpp
@@ -16,21 +16,14 @@ surface_temperature::surface_temperature(config_file cfg) : module_base("surface
 };
 void surface_temperature::init(mesh& domain)
 {
-	// enforces all access to data via cache is through face rather than the cache.
-	// automatically ends this option when it goes out of scope
-	auto guard = init_phase();
-
     for (size_t i = 0; i < domain->size_faces(); i++)
     {
         auto face = domain->face(i);
         auto& d = face->make_module_data<surface_temperature::data>(ID);
-
-        d.RCC = std::make_unique<RCC_surface_temperature<surface_temperature::API>>(d.api);
-
-        d.luce_tarboton = std::make_unique<luce_tarboton_surface_temperature<surface_temperature::API>>(d.api);
         
-        d.api.set_face(face);
-        d.api.set_global(global_param_);
+        d.mean_temperature.bind_target((*face)["air_temperature"_s]);
+        d.set_face(face);
+        d.set_global(global_param_);
     };
 };
 
@@ -41,73 +34,85 @@ void surface_temperature::run(mesh_elem& face)
     double swe = (*face)["swe"_s];
 
     if (swe == 0.0)
-        d.RCC->execute();
+        RCC.execute(d);
     else
-        d.luce_tarboton->execute();
+        luce_tarboton.execute(d);
 
-    d.api.set_outputs_to_face();
+    d.mean_temperature.accumulate(is_new_day(),steps_per_day());
+
+    d.set_outputs_to_face();
 };
 
-double& surface_temperature::API::air_temperature() const
+double& surface_temperature::data::air_temperature() const
 {
-	if (std::isnan(air_temperature)) 
-		air_temperature = (*face)["air_temperature"_s];
+    update_field(cache_->air_temperature,
+            [this]() { return (*face)["air_temperature"_s]; }
+            );
 
-	return air_temperature;
+    return cache_->air_temperature;
 };
 
-double& surface_temperature::API::thaw_front_depth() const
+double& surface_temperature::data::thaw_front_depth() const
 {
-	if (std::isnan(thaw_front_depth)) 
-		thaw_front_depth = (*face)["thaw_front_depth"_s];
+    update_field(cache_->thaw_front_depth,
+            [this]() { return (*face)["thaw_front_depth"_s]; }
+            );
 
-	return thaw_front_depth;
+    return cache_->thaw_front_depth;
 };
 
-double& surface_temperature::API::snow_depth() const
+double& surface_temperature::data::snow_depth() const
 {
-	if (std::isnan(snow_depth)) 
-		snow_depth = (*face)["snow_depth"_s];
+    update_field(cache_->snow_depth,
+            [this]() { return (*face)["snow_depth"_s]; }
+            );
 
-	return snow_depth;
+    return cache_->snow_depth;
 };
 
-double& surface_temperature::API::snow_density() const
+double& surface_temperature::data::snow_density() const
 {
-	if (std::isnan(snow_density)) 
-		snow_density = (*face)["snow_density"_s];
+    update_field(cache_->snow_density,
+            [this]() { return (*face)["snow_density"_s]; }
+            );
 
-	return snow_density;
+    return cache_->snow_density;
 };
 
-double& surface_temperature::API::ground_heat_flux() const
+double& surface_temperature::data::ground_heat_flux() const
 {
-	if (std::isnan(ground_heat_flux)) 
-		ground_heat_flux = (*face)["ground_heat_flux"_s];
+    update_field(cache_->ground_heat_flux,
+            [this]() { return (*face)["ground_heat_flux"_s]; }
+            );
 
-	return ground_heat_flux;
+    return cache_->ground_heat_flux;
 };
 
-double& surface_temperature::API::net_radiation() const
+double& surface_temperature::data::daily_mean_temperature() const
 {
-	if (std::isnan(net_radiation)) 
-		net_radiation = (*face)["net_radiation"_s];
+    return mean_temperature.get_last_mean();
+};
 
-	return net_radiation;
+double& surface_temperature::data::net_radiation() const
+{
+    update_field(cache_->net_radiation,
+            [this]() { return (*face)["net_radiation"_s]; }
+            );
+
+    return cache_->net_radiation;
 }
 
-
-void surface_temperature::API::surface_temperature(const double& value)
+void surface_temperature::data::surface_temperature(const double& value)
 {
-	surface_temperature = value;
+    set_output(cache_->surface_temperature,value);
 };
 
-void surface_temperature::API::snow_thermal_conductivity(const double& value)
+void surface_temperature::data::snow_thermal_conductivity(const double& value)
 {
-	snow_thermal_conductivity = value;
+    set_output(cache_->snow_thermal_conductivity, value);
 };
 
-void surface_temperature::API::set_outputs_to_face()
+void surface_temperature::data::set_outputs_to_face()
 {
 	(*face)["surface_temperature"_s] = surface_temperature;
 	
@@ -116,20 +121,7 @@ void surface_temperature::API::set_outputs_to_face()
 	reset_cache();
 };
 
-void surface_temperature::API::reset_cache();
-{
-	air_temperature = std::numeric_limits<double>::quiet_nan();
-	thaw_front_depth = std::numeric_limits<double>::quiet_nan();
-	snow_depth = std::numeric_limits<double>::quiet_nan();
-	snow_density = std::numeric_limits<double>::quiet_nan();
-	ground_heat_flux = std::numeric_limits<double>::quiet_nan();
-	net_radiation = std::numeric_limits<double>::quiet_nan();
-
-	surface_temperature = 0.0;
-	snow_thermal_conductivity = 0.0;
-};
-
-bool surface_temperature::API::is_new_day()
+bool surface_temperature::is_new_day()
 {
     int td = global_param->posix_time().time_of_day().total_seconds();
     int time_to_midnight = 86400 - td;
@@ -141,7 +133,7 @@ bool surface_temperature::API::is_new_day()
         return false;
 };
 
-int surface_temperature::API::steps_per_day()
+int surface_temperature::steps_per_day()
 {
 	static const auto [S,D] = [this]() ->std::pair<int, double>
 	{
@@ -155,6 +147,6 @@ int surface_temperature::API::steps_per_day()
 
 		return {s,d};
 	}();
-
-	return s / d;
+    
+	return S / D;
 };
