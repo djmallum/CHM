e9bb68d01f1059a5346a1792f1c6f26b7109f243
Allum <donovan.allum@usask.ca>
2025-08-06 12:37:11 -0600

    New CHM module soil_water_unit_conversion
    Computes:
    
    1. volumetric water content
    
    2. degree of saturation "saturation"
    
    Volumetric water content is defined as the volume of water divided by
    the volume of the soil, where the volume of the soil is the sum of the
    volume of water, air and minerals. In order words, it is the fraction of
    the soil that is water.
    
    The module soil_module computes soil moisture content, soil_storage.
    However, it only some of the moisture. In particular, it includes
    moisture above the wilt point so in order to get the volumetric water
    content and degree of saturation this must be accounted for.
    
    The submodule soil_moisture_converter accounts for this. Setting
    d.fractional_cutoff() > 0.0 achieves this.

---
+++ src/modules/soil_water_unit_conversion.cpp
@@ -0,0 +1,102 @@
+#include "soil_water_unit_conversion.hpp"
+#include <stdexcept>
+
+soil_water_unit_converter::soil_water_unit_converter(config_file cfg)
+    : module_base("soil_water_unit_converter", parallel::data, cfg)
+{
+    depends("soil_storage");
+
+    provides("soil_volumetric_content");
+    provides("soil_saturation");
+    provides("soil_storage_at_freeze");
+};
+
+double soil_water_unit_converter::data::soil_storage() const 
+{
+    update_field(cache_->soil_storage,
+           [this]() { return (*face)["soil_storage"_s]; } );
+
+    return cache_->soil_storage;
+};
+
+void soil_water_unit_converter::init(mesh& domain)
+{
+    for (size_t i = 0; i < domain->size_local_faces(); i++)
+    {
+        // Doing nothing except creating the data objects
+        auto face = domain->face(i);
+        auto& d = face->make_module_data<soil_water_unit_converter::data>(ID, face, global_param, cfg);
+    };
+};
+
+void soil_water_unit_converter::run(mesh_elem& face)
+{
+    // Constructor is empty because only fetching...
+    auto& d = face->get_module_data<soil_water_unit_converter::data>(ID);
+
+    converter.execute(d);
+};
+
+bool soil_water_unit_converter::data::storage_is_total_moisture() { return false; };
+
+double soil_water_unit_converter::data::fractional_cutoff()
+{
+    return SoilDataObj.wilt_point(get_soil_type());
+};
+
+const double soil_water_unit_converter::data::soil_storage_max()
+{
+    if (!soil_storage_max_)
+    {
+        soil_storage_max_.emplace(face->soil_attribute<double>("soil_storage_max"));
+    }
+    return *soil_storage_max_;
+};
+
+double soil_water_unit_converter::data::porosity()
+{
+    return SoilDataObj.porosity(get_soil_type());
+};
+
+void soil_water_unit_converter::data::volumetric_moisture_content(const double out) const
+{
+    set_output(cache_->volumetric_moisture_content,out); 
+};
+
+double soil_water_unit_converter::data::volumetric_moisture_content() const
+{
+    auto static err = 
+        []() {  throw std::runtime_error("volumetric_moisture_content accessed before being set");
+                return 1.0; };
+    
+    update_field(cache_->volumetric_moisture_content,err);
+
+    return cache_->volumetric_moisture_content;
+};
+
+void soil_water_unit_converter::data::saturation(const double out) const 
+{
+    set_output(cache_->saturation,out); 
+};
+
+double soil_water_unit_converter::data::saturation() const
+{
+    auto static err = 
+       []() { throw std::runtime_error("saturation accessed before being set");
+              return 1.0;   };
+
+    update_field(cache_->saturation,err);
+
+    return cache_->saturation;
+};
+
+std::string& soil_water_unit_converter::data::get_soil_type()
+{
+    if (!soil_type)
+    {
+        soil_type = cfg_.get<std::string>("soil_type");
+    }
+
+    return *soil_type;
+};
+
